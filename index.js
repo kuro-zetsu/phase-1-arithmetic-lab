49e34de737bc2c7fa7dc012889c95a0a65b13901
Reagan Nyauma <jreagan2525@gmail.com>
2025-03-23 08:00:01 +0300

    Created required variables and performed some artithmetic operations.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(1, 10, 13, 4, 20, 18);
